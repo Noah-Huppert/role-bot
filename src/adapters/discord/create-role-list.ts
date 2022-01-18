bc884a748dc132458a29e4c76d9f8b603e9d9c63
Noah Huppert <contact@noahh.io>
2022-01-17 23:56:00 -0500

    moved create new role handler to class
    This makes the create new role description more clean, and the adapter
    logic for separated.

--- src/adapters/discord/create-role-list.ts
+++ src/adapters/discord/create-role-list.ts
@@ -9,7 +9,6 @@ import { RoleManager } from "../../ports/roles";
 import {
   InteractionDescription,
   newCommandDescription,
-  newSelectMenuDescription,
 } from "./index";
 
 /**
@@ -28,16 +27,16 @@ type CreateRoleListOpts = {
  */
 export class CreateRoleListDescriber {
   /**
-   * The role manager port used by the handler.
+   * The options used by the handler.
    */
-  roleManager: RoleManager;
+  opts: CreateRoleListOpts;
 
   /**
    * Initialize the describer.
    * @param opts - Dependencies used by handler logic.
    */
   constructor(opts: CreateRoleListOpts) {
-    this.roleManager = opts.roleManager;
+    this.opts = opts;
   }
   
   /**
@@ -48,45 +47,47 @@ export class CreateRoleListDescriber {
       newCommandDescription({
         name: "create-role-lists",
         description: "Create a new list of roles from which users can self assign",
-        factory: () => {
-          return {
-            onCommand: async (cmd: CommandInteraction): Promise<void> => {
-              // Get roles
-              const roles = await this.roleManager.listRoles();
-
-              // Reply
-              await cmd.reply({
-                content: "Manage roles",
-                components: [
-                  new MessageActionRow({
-                    components: [
-                      new MessageSelectMenu({
-                        customId: "create-role-list:select_role",
-                        options: roles.map((role) => {
-                          return {
-                            label: role.name,
-                            description: role.description,
-                            value: role.name,
-                          };
-                        }),
-                      }),
-                    ],
-                  })
-                ]});
-            },
-          };
-        },
-      }),
-      newSelectMenuDescription({
-          customID: "create-role-list:select_role",
-          factory: () => {
-            return {
-              onSelectMenu: async (selectMenu: SelectMenuInteraction): Promise<void> => {
-                console.log("select", selectMenu);
-              },
-            };
-          },
+        factory: () => new CreateRoleListHandler(this.opts),
       }),
     ];
+  }
+}
+
+/**
+ * Handles a Discord create role list command interaction.
+ */
+class CreateRoleListHandler {
+  /**
+   * Prismatic dependencies used by the handler.
+   */
+  opts: CreateRoleListOpts;
+
+  constructor(opts: CreateRoleListOpts) {
+    this.opts = opts;
+  }
+
+  async onCommand(cmd: CommandInteraction): Promise<void> {
+    // Get roles
+    const roles = await this.opts.roleManager.listRoles();
+
+    // Reply
+    await cmd.reply({
+      content: "Manage roles",
+      components: [
+        new MessageActionRow({
+          components: [
+            new MessageSelectMenu({
+              customId: "create-role-list:select_role",
+              options: roles.map((role) => {
+                return {
+                  label: role.name,
+                  description: role.description,
+                  value: role.name,
+                };
+              }),
+            }),
+          ],
+        })
+      ]});
   }
 }
