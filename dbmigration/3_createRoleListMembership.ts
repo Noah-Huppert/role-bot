7883b54b53adc6bb7caf6a04250ec980c0e4ecc1
Noah Huppert <contact@noahh.io>
2022-04-23 15:51:12 -0400

    Created role list membership migration

---
+++ dbmigration/3_createRoleListMembership.ts
@@ -0,0 +1,17 @@
+import { EastPGClient } from "east-postgres";
+
+export const tags = [];
+
+export async function migrate(client: EastPGClient) {
+  await client.db.query(`CREATE TABLE role_list_membership (
+id           SERIAL PRIMARY KEY,
+role_id      INTEGER REFERENCES role(id),
+role_list_id INTEGER REFERENCES role_list(id),
+emoji        TEXT NOT NULL,
+UNIQUE(role_list_id, emoji)
+)`);
+}
+
+export async function rollback(client: EastPGClient) {
+  await client.db.query(`DROP TABLE role_list_membership`);
+}
