1576c5bcf1d06082eb1e5a48d194a3e824fd087d
Noah Huppert <contact@noahh.io>
2023-06-24 16:59:10 -0400

    api: got cmd registration working

--- api/discord/adapter.go
+++ api/discord/adapter.go
@@ -67,10 +67,23 @@ func UserErrorEmbed(e services.UserError) *discordgo.MessageEmbed {
 	}
 }
 
+/* // FlattenCmdIDs takes a list of commands and their sub-commands and returns a map where keys are command and sub-command IDs and value are
+func FlattenCmdIDs(cmds []*discordgo.ApplicationCommand) map[string]bool {
+	var ids = map[string]bool{}
+
+	for _, cmd := range cmds {
+		ids[cmd.ID] = true
+
+		for _, opt := range cmd.Options {
+
+		}
+	}
+} */
+
 // Sets up commands and command handlers.
 func (a *DiscordAdapter) Setup() error {
 	// Register slash commands
-	cmds, err := a.discord.ApplicationCommandBulkOverwrite(a.cfg.ClientID, a.cfg.GuildID, []*discordgo.ApplicationCommand{
+	createdCmds, err := a.discord.ApplicationCommandBulkOverwrite(a.cfg.ClientID, a.cfg.GuildID, []*discordgo.ApplicationCommand{
 		{
 			Type:        discordgo.ChatApplicationCommand,
 			Name:        "role-list",
@@ -95,8 +108,29 @@ func (a *DiscordAdapter) Setup() error {
 	if err != nil {
 		return fmt.Errorf("failed to register slash commands: %s", err)
 	}
-	for _, cmd := range cmds {
-		a.logger.Debugf("registered command %s", cmd.Name)
+	var createdCmdIDs = map[string]bool{}
+
+	for _, cmd := range createdCmds {
+		a.logger.Debugf("registered command %s (%s)", cmd.Name, cmd.ID)
+		createdCmdIDs[cmd.ID] = true
+	}
+
+	// Delete an old slash commands
+	allCmds, err := a.discord.ApplicationCommands(a.cfg.ClientID, a.cfg.GuildID)
+	if err != nil {
+		return fmt.Errorf("failed to list all slash commands: %s", err)
+	}
+
+	for _, cmd := range allCmds {
+		a.logger.Debugf("allCmd name=%s, id=%s", cmd.Name, cmd.ID)
+
+		if _, desiredCmd := createdCmdIDs[cmd.ID]; !desiredCmd {
+			if err := a.discord.ApplicationCommandDelete(a.cfg.ClientID, a.cfg.GuildID, cmd.ID); err != nil {
+				return fmt.Errorf("failed to delete old slash command named '%s': %s", cmd.Name, err)
+			} else {
+				a.logger.Debugf("deleted old command %s", cmd.Name)
+			}
+		}
 	}
 
 	a.logger.Info("setup complete")
