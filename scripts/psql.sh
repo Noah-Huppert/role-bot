b1e3f8f55e4ce37e39ad46ada45263745ae9e60b
Noah Huppert <contact@noahh.io>
2024-11-30 19:47:15 -0500

    made psql script pass args

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose exec -it postgres psql postgresql://devrolebot:devrolebot@postgres:5432/devrolebot
+docker-compose exec -it postgres psql postgresql://devrolebot:devrolebot@postgres:5432/devrolebot "$@"
